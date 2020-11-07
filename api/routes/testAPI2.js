81890792fbf0d11bbf75af3315d2265a2e070923
Patrick Li <lipatrick.1023@gmail.com>
2020-11-07 16:22:16 -0500

    Weeb API call test

--- api/routes/testAPI2.js
+++ api/routes/testAPI2.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    request.open('GET', 'https://jsonplaceholder.typicode.com/posts', true)
-    request.onload = function () {
-        // Begin accessing JSON data here
-        var data = JSON.parse(this.response)
-        
-        res.send(data);
-      }
-      // Send request
-      request.send()
+router.get('/', function (req, res, next) {
+    var request = require('request');
+    request('https://ghibliapi.herokuapp.com/films', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Begin accessing JSON data here
+            // var data = JSON.parse(response)
+
+            // data.forEach((movie) => {
+            //     // Log each movie's title
+            //     console.log(movie.title)
+            // })
+            var data = JSON.parse(body)
+            var list = ""
+            data.forEach((movie) => {
+                // console.log(movie.title)
+                list = list + "\n" + movie.title;
+            })
+            // console.log(body) // Print the google web page.
+            console.log(list)
+            res.send(body);
+        }
+    })
 });
 
 module.exports = router;
